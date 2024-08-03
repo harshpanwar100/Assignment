5ea54bf5a4de07dd69497056d9ff6f4948405f75
harshpanwar100 <harshpanwar3600@gmail.com>
2024-08-04 04:33:25 +0530

    commit-2

--- server.js
+++ server.js
@@ -15,44 +15,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.static('view'));
 
-function isAuthenticated(req, res, next) {
-    if (req.session.isAuthenticated) {
-        return next();
-    }
-    res.redirect('/adminlogin');
-}
-app.use('/dashboard', isAuthenticated);
-
-
 
 const PORT = process.env.PORT || 5000;
-
-
-
-
-//routes
-
-//app.get('/dashboard', (req,res) =>{
-  //  res.sendFile(path.join(__dirname,'view','adminhome.html'));
-//})
 
 
 //main coding of web app
 app.listen(PORT, () => {
     console.log('server is running on ',PORT);
 })
-console.log("hello motherfucker");
-
-/*const user = new User({
-    username : "user",
-    password : "password123"
-});
-user.save()*/
-
-
-/*const admin = new Admin({
-    adminname : "admin",
-    apassword : "admin123"
-});
-admin.save()*/
 
